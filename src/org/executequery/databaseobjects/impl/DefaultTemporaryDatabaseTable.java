2e64a6bbc0e505b231016d9fad0de687a6d170f6
Vasiliy Yashkov <vasiliy.yashkov@red-soft.ru>
2017-01-25 17:18:05 +0300

    Generating of sql statement for temporary table

--- src/org/executequery/databaseobjects/impl/DefaultTemporaryDatabaseTable.java
+++ src/org/executequery/databaseobjects/impl/DefaultTemporaryDatabaseTable.java
@@ -2,13 +2,21 @@ package org.executequery.databaseobjects.impl;
 
 import org.executequery.databaseobjects.DatabaseHost;
 import org.executequery.databaseobjects.DatabaseObject;
+import org.executequery.sql.SQLFormatter;
+import org.executequery.sql.StatementGenerator;
 import org.underworldlabs.jdbc.DataSourceException;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 
 /**
  * Created by vasiliy on 25.01.17.
  */
 public class DefaultTemporaryDatabaseTable extends DefaultDatabaseObject {
+
+    private DefaultDatabaseTable defaultDatabaseTable;
 
     public DefaultTemporaryDatabaseTable(DatabaseObject object) {
 
@@ -18,6 +26,8 @@ public class DefaultTemporaryDatabaseTable extends DefaultDatabaseObject {
         setSchemaName(object.getSchemaName());
         setName(object.getName());
         setRemarks(object.getRemarks());
+
+        defaultDatabaseTable = new DefaultDatabaseTable(object);
     }
 
     public DefaultTemporaryDatabaseTable(DatabaseHost host) {
@@ -27,7 +37,50 @@ public class DefaultTemporaryDatabaseTable extends DefaultDatabaseObject {
 
     public String getCreateSQLText() throws DataSourceException {
 
-        return "";
+        StatementGenerator statementGenerator = createStatementGenerator();
+        String databaseProductName = databaseProductName();
+        Statement statement = null;
+        int type = -1;
+        try {
+             statement = defaultDatabaseTable.getHost().getConnection().createStatement();
+
+            ResultSet resultSet = statement.executeQuery("Select RDB$RELATION_TYPE FROM RDB$RELATIONS R \n" +
+                    "WHERE R.RDB$RELATION_NAME = '" + defaultDatabaseTable.getName() + "'");
+
+            resultSet.next();
+            type = resultSet.getInt(1);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        } finally {
+            releaseResources(statement);
+        }
+
+        String createStatement =
+                statementGenerator.createTableWithConstraints(databaseProductName, defaultDatabaseTable);
+
+        createStatement = formatSqlText(createStatement);
+
+        createStatement = createStatement.replace("CREATE TABLE", "CREATE GLOBAL TEMPORARY TABLE");
+
+        createStatement = createStatement.substring(0, createStatement.length() - 2);
+
+        if (type == 4)
+            createStatement += " ON COMMIT PRESERVE ROWS;\n\n";
+        else if (type == 5)
+            createStatement += " ON COMMIT DELETE ROWS;\n\n";
+
+        StringBuilder sb = new StringBuilder();
+        sb.append(createStatement);
+        sb.append("\n\n");
+        sb.append(statementGenerator.tableConstraintsAsAlter(databaseProductName, defaultDatabaseTable));
+
+        return sb.toString();
+    }
+
+    private String formatSqlText(String text) {
+
+        return new SQLFormatter(text).format();
     }
 
     @Override

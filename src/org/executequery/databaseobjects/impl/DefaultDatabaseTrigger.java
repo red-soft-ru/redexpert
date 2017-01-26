83db07d7f134a87bae1200d7c1484bd8fbe75d1b
Vasiliy Yashkov <vasiliy.yashkov@red-soft.ru>
2017-01-26 12:19:50 +0300

    Implementation of default database trigger class

--- src/org/executequery/databaseobjects/impl/DefaultDatabaseTrigger.java
+++ src/org/executequery/databaseobjects/impl/DefaultDatabaseTrigger.java
@@ -1,7 +1,50 @@
 package org.executequery.databaseobjects.impl;
 
+import org.executequery.databaseobjects.DatabaseMetaTag;
+import org.executequery.databaseobjects.DatabaseProcedure;
+
 /**
  * Created by vasiliy on 26.01.17.
  */
-public class DefaultDatabaseTrigger {
+public class DefaultDatabaseTrigger extends DefaultDatabaseExecutable
+        implements DatabaseProcedure {
+    /**
+     * Creates a new instance.
+     */
+    public DefaultDatabaseTrigger() {}
+
+    /**
+     * Creates a new instance.
+     */
+    public DefaultDatabaseTrigger(DatabaseMetaTag metaTagParent, String name) {
+        super(metaTagParent, name);
+    }
+
+    /**
+     * Creates a new instance with
+     * the specified values.
+     */
+    public DefaultDatabaseTrigger(String schema, String name) {
+        setName(name);
+        setSchemaName(schema);
+    }
+
+    /**
+     * Returns the database object type.
+     *
+     * @return the object type
+     */
+    public int getType() {
+        return TRIGGER;
+    }
+
+    /**
+     * Returns the meta data key name of this object.
+     *
+     * @return the meta data key name.
+     */
+    public String getMetaDataKey() {
+        return META_TYPES[TRIGGER];
+    }
+
 }

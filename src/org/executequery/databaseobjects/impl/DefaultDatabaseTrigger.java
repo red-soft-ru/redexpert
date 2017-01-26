95d96465d78a02d29959ea56f70c369b001c0309
Vasiliy Yashkov <vasiliy.yashkov@red-soft.ru>
2017-01-26 14:55:59 +0300

    Getting of trigger source code

--- src/org/executequery/databaseobjects/impl/DefaultDatabaseTrigger.java
+++ src/org/executequery/databaseobjects/impl/DefaultDatabaseTrigger.java
@@ -2,12 +2,21 @@ package org.executequery.databaseobjects.impl;
 
 import org.executequery.databaseobjects.DatabaseMetaTag;
 import org.executequery.databaseobjects.DatabaseProcedure;
+import org.firebirdsql.jdbc.FBDatabaseMetaData;
+import org.firebirdsql.jdbc.FirebirdDatabaseMetaData;
+import org.underworldlabs.jdbc.DataSourceException;
+
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
 
 /**
  * Created by vasiliy on 26.01.17.
  */
 public class DefaultDatabaseTrigger extends DefaultDatabaseExecutable
         implements DatabaseProcedure {
+
+    private String triggerSourceCode;
+
     /**
      * Creates a new instance.
      */
@@ -45,6 +54,39 @@ public class DefaultDatabaseTrigger extends DefaultDatabaseExecutable
      */
     public String getMetaDataKey() {
         return META_TYPES[TRIGGER];
+    }
+
+    public String getTriggerSourceCode() {
+        if (!isMarkedForReload() && triggerSourceCode != null) {
+
+            return triggerSourceCode;
+        }
+
+        triggerSourceCode = "";
+
+        try {
+
+            DatabaseMetaData dmd = getMetaTagParent().getHost().getDatabaseMetaData();
+
+            String _catalog = getCatalogName();
+            String _schema = getSchemaName();
+
+
+            if (dmd instanceof FBDatabaseMetaData) {
+                FirebirdDatabaseMetaData fbMetaData = (FirebirdDatabaseMetaData)dmd;
+                triggerSourceCode = fbMetaData.getTriggerSourceCode(getName());
+            }
+
+            return triggerSourceCode;
+
+        } catch (SQLException e) {
+
+            throw new DataSourceException(e);
+
+        } finally {
+
+            setMarkedForReload(false);
+        }
     }
 
 }

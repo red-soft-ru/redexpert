69cfea4432ec64058d62a3b4b931d3b2ca3f44dc
Vasiliy Yashkov <vasiliy.yashkov@red-soft.ru>
2017-01-27 16:10:27 +0300

    [ui] Added sequence panel. See #14283

---
+++ src/org/executequery/gui/browser/BrowserSequencePanel.java
@@ -0,0 +1,25 @@
+package org.executequery.gui.browser;
+
+import org.executequery.gui.forms.AbstractFormObjectViewPanel;
+
+import java.awt.print.Printable;
+
+/**
+ * Created by vasiliy on 27.01.17.
+ */
+public class BrowserSequencePanel extends AbstractFormObjectViewPanel {
+    @Override
+    public void cleanup() {
+        
+    }
+
+    @Override
+    public Printable getPrintable() {
+        return null;
+    }
+
+    @Override
+    public String getLayoutName() {
+        return null;
+    }
+}

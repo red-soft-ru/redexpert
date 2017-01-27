43f47405f5f8ddbffd694051d9a2d959d66472e2
Vasiliy Yashkov <vasiliy.yashkov@red-soft.ru>
2017-01-27 15:31:25 +0300

    Sequence class implementation. See #14283

--- src/org/executequery/databaseobjects/impl/DefaultDatabaseSequence.java
+++ src/org/executequery/databaseobjects/impl/DefaultDatabaseSequence.java
@@ -1,7 +1,51 @@
 package org.executequery.databaseobjects.impl;
 
+import org.executequery.databaseobjects.DatabaseMetaTag;
+import org.executequery.databaseobjects.DatabaseProcedure;
+
 /**
  * Created by vasiliy on 27.01.17.
  */
-public class DefaultDatabaseSequence {
+public class DefaultDatabaseSequence extends DefaultDatabaseExecutable
+        implements DatabaseProcedure {
+    
+    /**
+     * Creates a new instance.
+     */
+    public DefaultDatabaseSequence() {}
+
+    /**
+     * Creates a new instance.
+     */
+    public DefaultDatabaseSequence(DatabaseMetaTag metaTagParent, String name) {
+        super(metaTagParent, name);
+    }
+
+    /**
+     * Creates a new instance with
+     * the specified values.
+     */
+    public DefaultDatabaseSequence(String schema, String name) {
+        setName(name);
+        setSchemaName(schema);
+    }
+
+    /**
+     * Returns the database object type.
+     *
+     * @return the object type
+     */
+    public int getType() {
+        return SEQUENCE;
+    }
+
+    /**
+     * Returns the meta data key name of this object.
+     *
+     * @return the meta data key name.
+     */
+    public String getMetaDataKey() {
+        return META_TYPES[SEQUENCE];
+    }
+
 }

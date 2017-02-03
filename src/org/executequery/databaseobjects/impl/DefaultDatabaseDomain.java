58ab0c5f66496eb34fa70075d2a44a49d32f9e28
Vasiliy Yashkov <vasiliy.yashkov@red-soft.ru>
2017-02-03 15:55:16 +0300

    Added method to get ddl for domain. See #14283

--- src/org/executequery/databaseobjects/impl/DefaultDatabaseDomain.java
+++ src/org/executequery/databaseobjects/impl/DefaultDatabaseDomain.java
@@ -1,6 +1,9 @@
 package org.executequery.databaseobjects.impl;
 
 import com.sun.org.apache.xalan.internal.xsltc.DOM;
+import liquibase.change.AddColumnConfig;
+import liquibase.change.ColumnConfig;
+import liquibase.change.core.AddColumnChange;
 import org.executequery.databaseobjects.DatabaseColumn;
 import org.executequery.databaseobjects.DatabaseMetaTag;
 import org.executequery.databaseobjects.DatabaseProcedure;
@@ -38,6 +41,8 @@ public class DefaultDatabaseDomain extends DefaultDatabaseExecutable
     //  private static final int cstring_type = 40;
     private static final int blob_type = 261;
     private static final short boolean_type = 23;
+
+    int sqlType, sqlSubtype, sqlSize, sqlScale;
 
     private List<DatabaseColumn> columns;
 
@@ -100,21 +105,18 @@ public class DefaultDatabaseDomain extends DefaultDatabaseExecutable
             if (dmd instanceof FBDatabaseMetaData) {
                 statement = dmd.getConnection().createStatement();
 
-                ResultSet rs = statement.executeQuery("SELECT first 1 " +
-                        "cast(RF.RDB$RELATION_NAME as varchar(63)) " +
-                        "AS RELATION_NAME,\n" +
+                ResultSet rs = statement.executeQuery("SELECT first 1\n" +
                         "cast(F.RDB$FIELD_NAME as varchar(63)) AS FIELD_NAME,\n" +
-                        "F.RDB$FIELD_TYPE AS FIELD_TYPE," +
+                        "F.RDB$FIELD_TYPE AS FIELD_TYPE,\n" +
                         "F.RDB$FIELD_SUB_TYPE AS FIELD_SUB_TYPE,\n" +
                         "F.RDB$FIELD_PRECISION AS FIELD_PRECISION,\n" +
                         "F.RDB$FIELD_SCALE AS FIELD_SCALE,\n" +
                         "F.RDB$FIELD_LENGTH AS FIELD_LENGTH,\n" +
                         "F.RDB$CHARACTER_LENGTH AS CHAR_LEN,\n" +
                         "F.RDB$DESCRIPTION AS REMARKS,\n" +
-                        "RF.RDB$DEFAULT_SOURCE AS DEFAULT_SOURCE,\n" +
+                        "F.RDB$DEFAULT_SOURCE AS DEFAULT_SOURCE,\n" +
                         "F.RDB$DEFAULT_SOURCE AS DOMAIN_DEFAULT_SOURCE,\n" +
-                        "RF.RDB$FIELD_POSITION + 1 AS FIELD_POSITION,\n" +
-                        "CAST(NULL AS SMALLINT) AS NULL_FLAG,\n" +
+                        "F.RDB$NULL_FLAG AS NULL_FLAG,\n" +
                         "F.RDB$NULL_FLAG AS SOURCE_NULL_FLAG,\n" +
                         "F.RDB$COMPUTED_BLR AS COMPUTED_BLR,\n" +
                         "F.RDB$CHARACTER_SET_ID,\n" +
@@ -122,23 +124,28 @@ public class DefaultDatabaseDomain extends DefaultDatabaseExecutable
                         "CAST(NULL AS VARCHAR(10)) AS JB_IDENTITY_TYPE\n" +
                         "FROM RDB$RELATION_FIELDS RF,RDB$FIELDS F\n" +
                         "WHERE\n" +
-                        "TRIM(F.RDB$FIELD_NAME) = '" + getName() + "'\n" +
-                        "and  RF.RDB$FIELD_SOURCE = F.RDB$FIELD_NAME\n" +
-                        "order by RF.RDB$RELATION_NAME, RF.RDB$FIELD_POSITION");
+                        "TRIM(F.RDB$FIELD_NAME) = '" + getName() + "'");
                 while (rs.next()) {
 
                     DefaultDatabaseColumn column = new DefaultDatabaseColumn();
                     column.setCatalogName(_catalog);
                     column.setSchemaName(_schema);
-                    column.setName(rs.getString(2));
-                    column.setTypeInt(rs.getInt(3));
-                    column.setTypeName(getDataTypeName(rs.getInt(3), rs.getInt(4), rs.getInt(6)));
-                    column.setColumnSize(rs.getInt(7));
-                    column.setColumnScale(rs.getInt(6));
-                    column.setRequired(rs.getInt(13) == DatabaseMetaData.columnNoNulls);
-                    column.setRemarks(rs.getString(9));
-                    this.setRemarks(rs.getString(9));
+                    column.setName(rs.getString(1));
+                    column.setTypeInt(rs.getInt(2));
+                    column.setTypeName(getDataTypeName(rs.getInt(2), rs.getInt(3), rs.getInt(5)));
+                    column.setColumnSize(rs.getInt(6));
+                    if (rs.getInt(4) != 0)
+                        column.setColumnSize(rs.getInt(4));
+                    column.setColumnScale(rs.getInt(5));
+                    column.setRequired(rs.getInt(12) == DatabaseMetaData.columnNoNulls);
+                    column.setRemarks(rs.getString(8));
+                    this.setRemarks(rs.getString(8));
 //                    column.setDefaultValue(rs.getString(13));
+
+                    sqlType = rs.getInt(2);
+                    sqlSubtype = rs.getInt(3);
+                    sqlSize = column.getColumnSize();
+                    sqlScale = rs.getInt(5);
 
                     columns.add(column);
                 }
@@ -155,6 +162,79 @@ public class DefaultDatabaseDomain extends DefaultDatabaseExecutable
         } finally {
 
             setMarkedForReload(false);
+        }
+    }
+
+    @Override
+    public String getCreateSQLText() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Create domain \n\t");
+        sb.append(getName());
+        sb.append("\n");
+        sb.append("\t as \n\t");
+        sb.append(getTypeWithSize(sqlType, sqlSubtype, sqlSize, sqlScale));
+        sb.append(";");
+        return sb.toString();
+    }
+
+    private String getTypeWithSize(int sqltype, int sqlsubtype, int sqlsize, int sqlscale) {
+        switch (sqltype) {
+            case smallint_type:
+                if (sqlsubtype == SUBTYPE_NUMERIC || (sqlsubtype == 0 && sqlscale < 0))
+                    return "NUMERIC(" + sqlSize + ",0)";
+                else if (sqlsubtype == SUBTYPE_DECIMAL)
+                    return "DECIMAL("+ sqlSize + "," + sqlscale + ")";
+                else
+                    return "SMALLINT";
+            case integer_type:
+                if (sqlsubtype == SUBTYPE_NUMERIC || (sqlsubtype == 0 && sqlscale < 0))
+                    return "NUMERIC(" + sqlSize + ",0)";
+                else if (sqlsubtype == SUBTYPE_DECIMAL)
+                    return "DECIMAL("+ sqlSize + "," + sqlscale + ")";
+                else
+                    return "INTEGER";
+            case double_type:
+            case d_float_type:
+                if (sqlsubtype == SUBTYPE_NUMERIC || (sqlsubtype == 0 && sqlscale < 0))
+                    return "NUMERIC(" + sqlSize + ",0)";
+                else if (sqlsubtype == SUBTYPE_DECIMAL)
+                    return "DECIMAL("+ sqlSize + "," + sqlscale + ")";
+                else
+                    return "DOUBLE PRECISION";
+            case float_type:
+                return "FLOAT";
+            case char_type:
+                return "CHAR(" + sqlsize + ")";
+            case varchar_type:
+                return "VARCHAR(" + sqlsize + ")";
+            case timestamp_type:
+                return "TIMESTAMP";
+            case time_type:
+                return "TIME";
+            case date_type:
+                return "DATE";
+            case int64_type:
+                if (sqlsubtype == SUBTYPE_NUMERIC || (sqlsubtype == 0 && sqlscale < 0))
+                    return "NUMERIC(" + sqlSize + ",0)";
+                else if (sqlsubtype == SUBTYPE_DECIMAL)
+                    return "DECIMAL("+ sqlSize + "," + sqlscale + ")";
+                else
+                    return "BIGINT";
+            case blob_type:
+                if (sqlsubtype < 0)
+                    return "BLOB SUB_TYPE <0";
+                else if (sqlsubtype == 0)
+                    return "BLOB SUB_TYPE 0";
+                else if (sqlsubtype == 1)
+                    return "BLOB SUB_TYPE 1";
+                else
+                    return "BLOB SUB_TYPE " + sqlsubtype;
+            case quad_type:
+                return "ARRAY";
+            case boolean_type:
+                return "BOOLEAN";
+            default:
+                return "NULL";
         }
     }
 

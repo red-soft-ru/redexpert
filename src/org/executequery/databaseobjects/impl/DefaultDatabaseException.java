95cc5cddb8f6ee301fe0abc187eb9a814eac85ad
Vasiliy Yashkov <vasiliy.yashkov@red-soft.ru>
2017-02-13 11:09:31 +0300

    Simple database exception class implementation. See #14283

--- src/org/executequery/databaseobjects/impl/DefaultDatabaseException.java
+++ src/org/executequery/databaseobjects/impl/DefaultDatabaseException.java
@@ -1,7 +1,35 @@
 package org.executequery.databaseobjects.impl;
 
+import org.executequery.databaseobjects.DatabaseMetaTag;
+import org.executequery.databaseobjects.DatabaseProcedure;
+
 /**
  * Created by vasiliy on 13.02.17.
  */
-public class DefaultDatabaseException {
+public class DefaultDatabaseException extends DefaultDatabaseExecutable
+        implements DatabaseProcedure {
+    /**
+     * Creates a new instance.
+     */
+    public DefaultDatabaseException() {}
+
+    /**
+     * Creates a new instance.
+     */
+    public DefaultDatabaseException(DatabaseMetaTag metaTagParent, String name) {
+        super(metaTagParent, name);
+    }
+
+    /**
+     * Creates a new instance with
+     * the specified values.
+     */
+    public DefaultDatabaseException(String schema, String name) {
+        setName(name);
+        setSchemaName(schema);
+    }
+
+    public int getType() {
+        return EXCEPTION;
+    }
 }

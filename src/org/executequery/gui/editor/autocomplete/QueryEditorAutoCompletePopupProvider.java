164cf722a68abc50d4a67522009489ae9f6e067f
Vasiliy Yashkov <vasiliy.yashkov@red-soft.ru>
2017-03-15 10:41:50 +0300

    Fixed a columns list with a column prefix for a table without a alias

--- src/org/executequery/gui/editor/autocomplete/QueryEditorAutoCompletePopupProvider.java
+++ src/org/executequery/gui/editor/autocomplete/QueryEditorAutoCompletePopupProvider.java
@@ -368,8 +368,11 @@ public class QueryEditorAutoCompletePopupProvider implements AutoCompletePopupPr
             List<AutoCompleteListItem> itemsForTable =
                     selectionsFactory.buildItemsForTable(databaseHost, tableString.toUpperCase());
 
-            if (!itemsForTable.isEmpty())
-                return itemsForTable;
+            if (!itemsForTable.isEmpty()) {
+                itemsForTable =
+                        buildItemsStartingWithForList(itemsForTable, tables, wordPrefix, hasDotIndex);
+            }
+            return itemsForTable;
         }
 
         // maybe alias?

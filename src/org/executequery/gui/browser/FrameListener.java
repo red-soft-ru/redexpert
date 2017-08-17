4189906fe8a19ce807aa36c96322863d3c7d19e2
Unknown <kalyachin@mail.ru>
2017-08-17 21:50:53 +0300

    Added Frame with login and password if connection is not connected

---
+++ src/org/executequery/gui/browser/FrameListener.java
@@ -0,0 +1,51 @@
+package org.executequery.gui.browser;
+
+import javax.swing.*;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+
+public class FrameListener implements WindowListener {
+    public FrameListener(JFrame frame)
+    {
+        fram=frame;
+        fram.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
+
+    }
+    JFrame fram;
+    @Override
+    public void windowOpened(WindowEvent windowEvent) {
+
+    }
+
+    @Override
+    public void windowClosing(WindowEvent windowEvent) {
+        fram.dispose();
+
+    }
+
+    @Override
+    public void windowClosed(WindowEvent windowEvent) {
+
+
+    }
+
+    @Override
+    public void windowIconified(WindowEvent windowEvent) {
+
+    }
+
+    @Override
+    public void windowDeiconified(WindowEvent windowEvent) {
+
+    }
+
+    @Override
+    public void windowActivated(WindowEvent windowEvent) {
+
+    }
+
+    @Override
+    public void windowDeactivated(WindowEvent windowEvent) {
+
+    }
+}

1d1105509c6b6fcca60b947c7550f3286eeb73a5
Vasiliy Yashkov <vasiliy.yashkov@red-soft.biz>
2017-09-08 15:17:37 +0300

    Refactoring

--- src/org/executequery/gui/browser/managment/ThreadOfUserManager.java
+++ src/org/executequery/gui/browser/managment/ThreadOfUserManager.java
@@ -2,12 +2,14 @@ package org.executequery.gui.browser.managment;
 
 import org.executequery.gui.browser.UserManagerPanel;
 
-public class ThreadOfUserManager implements Runnable{
-    public ThreadOfUserManager(UserManagerPanel ump)
-    {super();
-        brp=ump;
+public class ThreadOfUserManager implements Runnable {
+    public ThreadOfUserManager(UserManagerPanel ump) {
+        super();
+        brp = ump;
     }
+
     UserManagerPanel brp;
+
     @Override
     public void run() {
 

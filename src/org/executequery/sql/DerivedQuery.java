a637cf472b7e350facc887d7cbc37d6ec943c900
Mikhail Kalyashin <kalyachin@mail.ru>
2018-02-01 10:05:31 +0300

    Fixed name operation

--- src/org/executequery/sql/DerivedQuery.java
+++ src/org/executequery/sql/DerivedQuery.java
@@ -187,7 +187,7 @@ public final class DerivedQuery {
             type = QueryTypes.CREATE_TRIGGER;
 
         } else if (query.indexOf("CREATE ") == 0 && (query.indexOf("PROCEDURE ") != -1 ||
-                query.indexOf("PACKAGE ") != -1)) {
+                query.indexOf("PACKAGE ") == -1)) {
 
             type = QueryTypes.CREATE_PROCEDURE;
 

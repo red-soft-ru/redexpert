eeb0f4e9242603d7aede673567a5cf1c132a45bf
mikhan808 <kalyachin@mail.ru>
2018-08-31 10:23:00 +0300

    fixed help for Windows (part2)

--- src/org/executequery/actions/helpcommands/HelpCommand.java
+++ src/org/executequery/actions/helpcommands/HelpCommand.java
@@ -23,7 +23,6 @@ package org.executequery.actions.helpcommands;
 import org.executequery.actions.AbstractUrlLauncherCommand;
 import org.underworldlabs.util.MiscUtils;
 
-import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -36,27 +35,19 @@ import java.net.URL;
 public class HelpCommand extends AbstractUrlLauncherCommand {
 
     private static final String URL = "guide/RedExpert_Guide-ru.pdf";
-    private static final String URL2 = "../guide/RedExpert_Guide-ru.pdf";
 
     @Override
     public String url() {
         java.net.URL[] urls = new URL[0];
-        File file = new File(URL);
-        if (file.exists()) {
+
             try {
                 urls = MiscUtils.loadURLs(URL);
             } catch (MalformedURLException e) {
                 e.printStackTrace();
             }
-        } else {
-            try {
-                urls = MiscUtils.loadURLs(URL2);
-            } catch (MalformedURLException e) {
-                e.printStackTrace();
-            }
-        }
-
-        return urls[0].toString();
+        String url = urls[0].toString();
+        url = url.replace("bin/guide/RedExpert_Guide-ru.pdf", "guide/RedExpert_Guide-ru.pdf");
+        return url;
     }
 
 }

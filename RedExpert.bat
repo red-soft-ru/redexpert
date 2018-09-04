0c3db582d677a0c821f67065467138e7af54daa5
Vasiliy Yashkov <vasiliy.yashkov@red-soft.biz>
2018-09-04 11:26:51 +0300

    Update scripts to run binaries

--- RedExpert.bat
+++ RedExpert.bat
@@ -1,3 +1,9 @@
 @echo off
 
-call bin\\RedExpertNativeLauncher64.exe -Xms1024m
+if exist "%SYSTEMDRIVE%\Program Files (x86)\" (
+	cd bin
+	call RedExpert64.exe -Xms1024m
+) else (
+	cd bin
+	call RedExpert.exe -Xms1024m
+)

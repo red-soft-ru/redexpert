b8eebf37a99584ade9bd14761e230e01a81af7b2
Vasiliy Yashkov <vasiliy.yashkov@red-soft.ru>
2019-03-18 10:33:42 +0300

    Connection check when reconnecting
    In query editor there is ability reset connection to database and reconnect. If there is no connection to database, an error will occur. Need to check connection, and if no available connections print  message.

--- src/org/executequery/gui/editor/autocomplete/QueryEditorAutoCompletePopupProvider.java
+++ src/org/executequery/gui/editor/autocomplete/QueryEditorAutoCompletePopupProvider.java
@@ -31,6 +31,7 @@ import org.executequery.log.Log;
 import org.executequery.repository.spi.KeywordRepositoryImpl;
 import org.executequery.sql.DerivedQuery;
 import org.executequery.sql.QueryTable;
+import org.executequery.sql.SqlMessages;
 import org.executequery.util.UserProperties;
 import org.underworldlabs.swing.util.SwingWorker;
 
@@ -126,6 +127,11 @@ public class QueryEditorAutoCompletePopupProvider implements AutoCompletePopupPr
     }
 
     public void reset() {
+
+        if (databaseHost == null) {
+            queryEditor.setOutputMessage(SqlMessages.PLAIN_MESSAGE, "Not Connected");
+            return;
+        }
 
         connectionChanged(databaseHost.getDatabaseConnection());
     }

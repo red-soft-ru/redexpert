1fff26f0a5d8fa817f8f5d7e26f7654a5b96b866
Vasiliy Yashkov <vasiliy.yashkov@red-soft.ru>
2019-03-29 16:23:27 +0300

    Removed redundant .sh and .bat

--- createDesktopEntry.sh
+++ createDesktopEntry.sh
@@ -5,7 +5,13 @@ if [ ! -f $HOME/.local/share/applications/redexpert.desktop ]; then
     if [[ $PWD == *"/bin"* ]]; then
         EXE_PATH=$(dirname "$EXE_PATH")
     fi
-    cp $EXE_PATH/redexpert.desktop $HOME/.local/share/applications/redexpert.desktop
-    echo "Exec=\"$EXE_PATH/RedExpert.sh\" $JAVA_HOME">>$HOME/.local/share/applications/redexpert.desktop
+    cp "$EXE_PATH/redexpert.desktop" $HOME/.local/share/applications/redexpert.desktop
+    if [ `getconf LONG_BIT` = "64" ]
+    then
+        echo "Exec=\"$EXE_PATH/bin/RedExpert64\"">>$HOME/.local/share/applications/redexpert.desktop
+    else
+        echo "Exec=\"$EXE_PATH/bin/RedExpert\"">>$HOME/.local/share/applications/redexpert.desktop
+    fi
+
     echo "Icon=$EXE_PATH/red_expert.png">>$HOME/.local/share/applications/redexpert.desktop
 fi

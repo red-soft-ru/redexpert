9d9e5a5d1eea8fe83e07091988751f5a3de6a25f
Vasiliy Yashkov <vasiliy.yashkov@red-soft.ru>
2019-03-29 17:45:53 +0300

    Moved authentication menu from tools in help menu
    Because it's out of place in tools menu.

--- src/org/executequery/actions/helpcommands/ExitFromAccountCommand.java
+++ src/org/executequery/actions/helpcommands/ExitFromAccountCommand.java
@@ -1,4 +1,4 @@
-package org.executequery.actions.toolscommands;
+package org.executequery.actions.helpcommands;
 
 import org.executequery.GUIUtilities;
 import org.executequery.actions.OpenFrameCommand;

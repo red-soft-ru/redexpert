dc0e40ecc18a56e9292c5748aab715b3f6e75617
Vasiliy Yashkov <vasiliy.yashkov@red-soft.ru>
2019-03-29 17:53:18 +0300

    Replace desktop shortcut if exists

--- createDesktopEntry.sh
+++ createDesktopEntry.sh
@@ -1,17 +1,15 @@
 #!/usr/bin/env bash
 
-if [ ! -f $HOME/.local/share/applications/redexpert.desktop ]; then
-    EXE_PATH=$PWD
-    if [[ $PWD == *"/bin"* ]]; then
-        EXE_PATH=$(dirname "$EXE_PATH")
-    fi
-    cp "$EXE_PATH/redexpert.desktop" $HOME/.local/share/applications/redexpert.desktop
-    if [ `getconf LONG_BIT` = "64" ]
-    then
-        echo "Exec=\"$EXE_PATH/bin/RedExpert64\"">>$HOME/.local/share/applications/redexpert.desktop
-    else
-        echo "Exec=\"$EXE_PATH/bin/RedExpert\"">>$HOME/.local/share/applications/redexpert.desktop
-    fi
-
-    echo "Icon=$EXE_PATH/red_expert.png">>$HOME/.local/share/applications/redexpert.desktop
+EXE_PATH=$PWD
+if [[ $PWD == *"/bin"* ]]; then
+    EXE_PATH=$(dirname "$EXE_PATH")
 fi
+yes | cp -rf "$EXE_PATH/redexpert.desktop" $HOME/.local/share/applications/redexpert.desktop
+if [ `getconf LONG_BIT` = "64" ]
+then
+    echo "Exec=\"$EXE_PATH/bin/RedExpert64\"">>$HOME/.local/share/applications/redexpert.desktop
+else
+    echo "Exec=\"$EXE_PATH/bin/RedExpert\"">>$HOME/.local/share/applications/redexpert.desktop
+fi
+
+echo "Icon=$EXE_PATH/red_expert.png">>$HOME/.local/share/applications/redexpert.desktop

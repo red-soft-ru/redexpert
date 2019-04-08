07c001e5a89d33e8a33e61039256b97e2d77e47b
mikhan808 <kalyachin@mail.ru>
2019-04-08 16:58:00 +0300

    See #35490 added check variable LICENSE_INSTALLBUILDER
    (cherry picked from commit d9503bdc791bff00f49d7b0d1c4881f60c9334f6)

--- ci/build_installer.ps1
+++ ci/build_installer.ps1
@@ -5,6 +5,11 @@ if (-Not(Test-Path Env:\RE_VERSION))
     echo "RE_VERSION environment variable required"
     exit 1
 }
+if (-Not(Test-Path Env:\LICENSE_INSTALLBUILDER))
+{
+    echo "LICENSE_INSTALLBUILDER environment variable required"
+    exit 1
+}
 $VERSION=$env:RE_VERSION
 echo "version=$VERSION"
 $DIST=$env:DIST
@@ -18,5 +23,5 @@ if ($ARCH -eq "amd64")
 echo "Src=$SRC_DIR"
 echo "Dist=$DIST"
 cd $SRC_DIR/installer
-$env:LICENSE_INSTALLBUILDER | out-file license.xml
-builder-cli.exe build redexpert.xml --license ~/license.xml --verbose --setvars redexpert_dir=$DIST output_dir=$DIST/bin installer_name=$INSTALLER_NAME execution_file=$exec_file VERSION=$VERSION
+$env:LICENSE_INSTALLBUILDER | out-file licenses/license.xml
+builder-cli.exe build redexpert.xml --license ~/licenses/license.xml --verbose --setvars redexpert_dir=$DIST output_dir=$DIST/bin installer_name=$INSTALLER_NAME execution_file=$exec_file VERSION=$VERSION

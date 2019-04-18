9a0cf4bd9f4c69b64a4acb0419f92b0c8215042b
mikhan808 <kalyachin@mail.ru>
2019-04-18 15:42:05 +0300

    See #34608 fixed names columns when editing constraint

--- src/org/executequery/gui/table/EditConstraintPanel.java
+++ src/org/executequery/gui/table/EditConstraintPanel.java
@@ -353,7 +353,7 @@ public class EditConstraintPanel extends AbstractCreateObjectPanel implements Ke
                         if (!first)
                             sb.append(", ");
                         first = false;
-                        sb.append(key);
+                        sb.append(MiscUtils.wordInQuotes(key));
                     }
                 }
                 sb.append(")");
@@ -375,7 +375,7 @@ public class EditConstraintPanel extends AbstractCreateObjectPanel implements Ke
                         if (!first)
                             sb.append(", ");
                         first = false;
-                        sb.append(key);
+                        sb.append(MiscUtils.wordInQuotes(key));
                     }
                 }
                 sb.append(")\n");
@@ -408,7 +408,7 @@ public class EditConstraintPanel extends AbstractCreateObjectPanel implements Ke
                         if (!first)
                             sb.append(", ");
                         first = false;
-                        sb.append(key);
+                        sb.append(MiscUtils.wordInQuotes(key));
                     }
                 }
                 sb.append(")");

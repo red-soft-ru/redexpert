7834c7d6d64ea7d824b954160e5d44e8abc9a429
mikhan808 <kalyachin@mail.ru>
2019-04-25 13:20:49 +0300

    fixed creating desktop entry

--- createDesktopEntry.sh
+++ createDesktopEntry.sh
@@ -7,9 +7,9 @@ fi
 yes | cp -rf "$EXE_PATH/redexpert.desktop" $HOME/.local/share/applications/redexpert.desktop
 if [ `getconf LONG_BIT` = "64" ]
 then
-    echo "Exec=\"$EXE_PATH/bin/RedExpert64\"">>$HOME/.local/share/applications/redexpert.desktop
+    echo "Exec=bash -c \" cd $EXE_PATH/bin && $EXE_PATH/bin/RedExpert64\"">>$HOME/.local/share/applications/redexpert.desktop
 else
-    echo "Exec=\"$EXE_PATH/bin/RedExpert\"">>$HOME/.local/share/applications/redexpert.desktop
+    echo "Exec=bash -c \" cd $EXE_PATH/bin && $EXE_PATH/bin/RedExpert\"">>$HOME/.local/share/applications/redexpert.desktop
 fi
 echo "Path=\"$EXE_PATH/bin\"">>$HOME/.local/share/applications/redexpert.desktop
 echo "Icon=$EXE_PATH/red_expert.png">>$HOME/.local/share/applications/redexpert.desktop

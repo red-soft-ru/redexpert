9bd83cc2f0b2d60d7cffa875b71450f4c6a38fa5
Vasiliy Yashkov <vasiliy.yashkov@red-soft.ru>
2019-05-08 17:06:35 +0300

    Typo

--- src/org/executequery/actions/helpcommands/ExitFromAccountCommand.java
+++ src/org/executequery/actions/helpcommands/ExitFromAccountCommand.java
@@ -11,7 +11,7 @@ import java.awt.event.ActionEvent;
 public class ExitFromAccountCommand extends OpenFrameCommand implements BaseCommand {
     @Override
     public void execute(ActionEvent e) {
-        if (GUIUtilities.displayConfirmDialog("Do yo want exit from this account?") == JOptionPane.YES_OPTION) {
+        if (GUIUtilities.displayConfirmDialog("Do you want exit from this account?") == JOptionPane.YES_OPTION) {
             SystemProperties.setStringProperty("user", "reddatabase.token", "");
             GUIUtilities.loadAuthorisationInfo();
         }

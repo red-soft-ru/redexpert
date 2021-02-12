3e7e55adabc8bef8506ee447f92ec86acf25567d
mikhan808 <kalyachin@mail.ru>
2021-02-12 15:19:31 +0300

    fix display of release notes in Russian(part 2)

--- src/org/executequery/CheckForUpdateNotifier.java
+++ src/org/executequery/CheckForUpdateNotifier.java
@@ -508,7 +508,7 @@ public class CheckForUpdateNotifier implements Interruptible {
 
             final String finalReleaseNotes = releaseNotes;
             GUIUtils.invokeAndWait(() -> new InformationDialog(bundledString("latestVersionInfoTitle"),
-                    finalReleaseNotes, InformationDialog.TEXT_CONTENT_VALUE, "utf8"));
+                    finalReleaseNotes, InformationDialog.TEXT_CONTENT_VALUE, null));
 
             return Constants.WORKER_SUCCESS;
 

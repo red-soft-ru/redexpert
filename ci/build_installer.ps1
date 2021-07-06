7e66e0939be98a6a8a44a4fc405eab67c7b09a68
mikhan808 <kalyachin@mail.ru>
2021-07-06 12:01:25 +0300

    Revert "fix installer build"
    This reverts commit 15fa5b59

--- ci/build_installer.ps1
+++ ci/build_installer.ps1
@@ -10,18 +10,18 @@ if (-Not(Test-Path Env:\LICENSE_INSTALLBUILDER_REDXPERT))
     echo "LICENSE_INSTALLBUILDER_REDXPERT environment variable required"
     exit 1
 }
-$DIST=$env:DIST
-$ARCH=$env:ARCH
-$VERSION=$env:RE_VERSION
+$DIST = $env:DIST
+$ARCH = $env:ARCH
+$VERSION = $env:RE_VERSION
 echo "version=$VERSION"
-$INSTALLER_NAME="RedXpert-$VERSION-installer-windows-$ARCH.exe"
-$exec_file="RedXpert"
+$INSTALLER_NAME = "RedXpert-$VERSION-installer-windows-$ARCH.exe"
+$exec_file = "RedXpert"
 if ($ARCH -eq "amd64")
 {
-    $exec_file="RedXpert64"
+    $exec_file = "RedXpert64"
 }
 echo "Src=$SRC_DIR"
 echo "Dist=$DIST"
 cd $SRC_DIR/installer
 $env:LICENSE_INSTALLBUILDER_REDXPERT | out-file license.xml
-builder-cli.exe build RedXpert.xml --license license.xml --verbose --setvars redexpert_dir=$DIST output_dir=$DIST/bin installer_name=$INSTALLER_NAME execution_file=$exec_file VERSION=$VERSION
+builder-cli.exe build RedXpert.xml --license license.xml --verbose --setvars redexpert_dir = $DIST output_dir = $DIST/bin installer_name = $INSTALLER_NAME execution_file = $exec_file VERSION = $VERSION

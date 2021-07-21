25d26a09126e1ff8b73b3d0737f24b42816526a7
mikhan808 <kalyachin@mail.ru>
2021-07-21 10:16:10 +0300

    Revert Rebranding

--- ci/build_installer.ps1
+++ ci/build_installer.ps1
@@ -10,18 +10,18 @@ if (-Not(Test-Path Env:\LICENSE_INSTALLBUILDER))
     echo "LICENSE_INSTALLBUILDER environment variable required"
     exit 1
 }
-$VERSION = $env:RE_VERSION
+$VERSION=$env:RE_VERSION
 echo "version=$VERSION"
-$DIST = $env:DIST
-$ARCH = $env:ARCH
-$INSTALLER_NAME = "RedExpert-$VERSION-installer-windows-$ARCH.exe"
-$exec_file = "RedExpert"
+$DIST=$env:DIST
+$ARCH=$env:ARCH
+$INSTALLER_NAME="RedExpert-$VERSION-installer-windows-$ARCH.exe"
+$exec_file="RedExpert"
 if ($ARCH -eq "amd64")
 {
-    $exec_file = "RedExpert64"
+    $exec_file="RedExpert64"
 }
 echo "Src=$SRC_DIR"
 echo "Dist=$DIST"
 cd $SRC_DIR/installer
 $env:LICENSE_INSTALLBUILDER | out-file license.xml
-builder-cli.exe build redexpert.xml --license license.xml --verbose --setvars redexpert_dir = $DIST output_dir = $DIST/bin installer_name = $INSTALLER_NAME execution_file = $exec_file VERSION = $VERSION
+builder-cli.exe build redexpert.xml --license license.xml --verbose --setvars redexpert_dir=$DIST output_dir=$DIST/bin installer_name=$INSTALLER_NAME execution_file=$exec_file VERSION=$VERSION

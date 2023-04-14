956957bf12b8c4baab1b63f8eb12539219ccba0b
aleksey.kozlov <aleksey.kozlov@red-soft.ru>
2023-04-14 20:59:03 +0300

    Add execution time converting

--- src/org/executequery/gui/browser/profiler/ProfilerTreeCellRenderer.java
+++ src/org/executequery/gui/browser/profiler/ProfilerTreeCellRenderer.java
@@ -18,13 +18,65 @@ class ProfilerTreeCellRenderer extends AbstractTreeCellRenderer {
         if (userObject instanceof ProfilerData) {
 
             ProfilerData data = (ProfilerData) userObject;
+            FormattedTime totalTime = new FormattedTime(data.getTotalTime());
+            FormattedTime avgTime = new FormattedTime(data.getAvgTime());
+
             String prefix = data.getCallCount() == 1 ?
-                    "[" + data.getTotalTime() + "ms]   " :
-                    "[" + data.getTotalTime() + "ms [" + data.getCallCount() + " times, ~" + data.getAvgTime() + "ms]]   ";
+                    String.format("[%s %s]   ",
+                            totalTime.getTime(), totalTime.getLabel()
+                    ) :
+                    String.format("[%s %s [%d times, ~%s %s]]   ",
+                            totalTime.getTime(), totalTime.getLabel(), data.getCallCount(), avgTime.getTime(), avgTime.getLabel()
+                    );
+
             String record = prefix + data.getProcessName();
             setText(record);
         }
 
         return this;
     }
+
+    private static class FormattedTime {
+
+        private static final long NS_MAX = 100000;
+        private static final long MS_MAX = NS_MAX * 10000;
+        private static final long S_MAX = MS_MAX * 60000;
+
+        private double time;
+        private String label;
+
+        FormattedTime (double time) {
+
+            this.time = time;
+            this.label = "ns";
+
+            if (time >= NS_MAX) {
+                if (time >= MS_MAX) {
+                    if (time >= S_MAX) {
+                        this.time /= S_MAX;
+                        this.label = "m";
+                    } else {
+                        this.time /= MS_MAX;
+                        this.label = "s";
+                    }
+                } else {
+                    this.time /= NS_MAX;
+                    this.label = "ms";
+                }
+            }
+
+        }
+
+        public String getTime() {
+            return String
+                    .format((label.equals("ms") || label.equals("ns") ? "%.0f" : "%.2f"), time)
+                    .replace(',', '.');
+        }
+
+        public String getLabel() {
+            return label;
+        }
+
+    }
+
 }

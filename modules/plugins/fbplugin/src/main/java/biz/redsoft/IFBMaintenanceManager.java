bc31a2bb9305c5bcc4e31a1830427798c3f2987b
aleksey.kozlov <aleksey.kozlov@red-soft.ru>
2023-06-06 19:37:56 +0300

    Add support of FBMaintenanceManager

---
+++ modules/plugins/fbplugin/src/main/java/biz/redsoft/IFBMaintenanceManager.java
@@ -0,0 +1,105 @@
+package biz.redsoft;
+
+import java.io.OutputStream;
+import java.sql.SQLException;
+import java.util.List;
+
+public interface IFBMaintenanceManager {
+
+    int ACCESS_MODE_READ_WRITE = 40;
+    int ACCESS_MODE_READ_ONLY = 39;
+    int SHUTDOWN_ATTACH = 9;
+    int SHUTDOWN_TRANSACTIONAL = 10;
+    int SHUTDOWN_FORCE = 7;
+    int VALIDATE_READ_ONLY = 16;
+    int VALIDATE_IGNORE_CHECKSUM = 32;
+    int VALIDATE_FULL = 128;
+    int PAGE_FILL_FULL = 35;
+    int PAGE_FILL_RESERVE = 36;
+    byte OPERATION_MODE_NORMAL = 0;
+    byte OPERATION_MODE_MULTI = 1;
+    byte OPERATION_MODE_SINGLE = 2;
+    byte OPERATION_MODE_FULL_SHUTDOWN = 3;
+    int SHUTDOWNEX_FORCE = 41;
+    int SHUTDOWNEX_ATTACHMENTS = 42;
+    int SHUTDOWNEX_TRANSACTIONS = 43;
+    int PARALLEL_WORKERS = 52;
+
+    void setDatabaseAccessMode(int var1) throws SQLException;
+
+    void setDatabaseDialect(int var1) throws SQLException;
+
+    void setDefaultCacheBuffer(int var1) throws SQLException;
+
+    void setForcedWrites(boolean var1) throws SQLException;
+
+    void setPageFill(int var1) throws SQLException;
+
+    void shutdownDatabase(int var1, int var2) throws SQLException;
+
+    void shutdownDatabase(byte var1, int var2, int var3) throws SQLException;
+
+    void bringDatabaseOnline() throws SQLException;
+
+    void bringDatabaseOnline(byte var1) throws SQLException;
+
+    void markCorruptRecords() throws SQLException;
+
+    void validateDatabase() throws SQLException;
+
+    void validateDatabase(int var1) throws SQLException;
+
+    void validateTable(String var1) throws SQLException;
+
+    void setParallelWorkers(int var1);
+
+    void setSweepThreshold(int var1) throws SQLException;
+
+    void sweepDatabase() throws SQLException;
+
+    void sweepDatabase(int var1) throws SQLException;
+
+    void activateShadowFile() throws SQLException;
+
+    void killUnavailableShadows() throws SQLException;
+
+    @Deprecated
+    void listLimboTransactions() throws SQLException;
+
+    List<Long> limboTransactionsAsList() throws SQLException;
+
+    long[] getLimboTransactions() throws SQLException;
+
+    void commitTransaction(long var1) throws SQLException;
+
+    void rollbackTransaction(long var1) throws SQLException;
+
+    String getCharSet();
+
+    void setCharSet(String var1);
+
+    String getUser();
+
+    void setUser(String var1);
+
+    String getPassword();
+
+    void setPassword(String var1);
+
+    String getDatabase();
+
+    void setDatabase(String var1);
+
+    String getHost();
+
+    void setHost(String var1);
+
+    int getPort();
+
+    void setPort(int var1);
+
+    OutputStream getLogger();
+
+    void setLogger(OutputStream var1);
+
+}

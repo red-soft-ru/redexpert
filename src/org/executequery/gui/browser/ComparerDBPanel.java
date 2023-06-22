e0f28fabef3cb152656195eb289958d16eb4eb01
aleksey.kozlov <aleksey.kozlov@red-soft.ru>
2023-06-22 14:06:31 +0300

    Add not empty tree nodes selection with children count display

--- src/org/executequery/gui/browser/ComparerDBPanel.java
+++ src/org/executequery/gui/browser/ComparerDBPanel.java
@@ -472,7 +472,7 @@ public class ComparerDBPanel extends JPanel implements TabView {
 
                     ((ComparerTreeNode) rootTreeNode.getChildAt(ComparerTreeNode.CREATE))
                             .add(new ComparerTreeNode(ComparerTreeNode.CREATE, type,
-                                    Bundles.get(NamedObject.class, NamedObject.META_TYPES_FOR_BUNDLE[type]), false));
+                                    Bundles.get(NamedObject.class, NamedObject.META_TYPES_FOR_BUNDLE[type]), ComparerTreeNode.TYPE_FOLDER));
 
                     loggingOutputPanel.append(MessageFormat.format("\n============= {0} to CREATE  =============",
                             Bundles.getEn(NamedObject.class, NamedObject.META_TYPES_FOR_BUNDLE[type])));
@@ -502,7 +502,7 @@ public class ComparerDBPanel extends JPanel implements TabView {
 
                     ((ComparerTreeNode) rootTreeNode.getChildAt(ComparerTreeNode.ALTER))
                             .add(new ComparerTreeNode(ComparerTreeNode.ALTER, type,
-                                    Bundles.get(NamedObject.class, NamedObject.META_TYPES_FOR_BUNDLE[type]), false));
+                                    Bundles.get(NamedObject.class, NamedObject.META_TYPES_FOR_BUNDLE[type]), ComparerTreeNode.TYPE_FOLDER));
 
                     loggingOutputPanel.append(MessageFormat.format("\n============= {0} to ALTER  =============",
                             Bundles.getEn(NamedObject.class, NamedObject.META_TYPES_FOR_BUNDLE[type])));
@@ -532,7 +532,7 @@ public class ComparerDBPanel extends JPanel implements TabView {
 
                     ((ComparerTreeNode) rootTreeNode.getChildAt(ComparerTreeNode.DROP))
                             .add(new ComparerTreeNode(ComparerTreeNode.DROP, type,
-                                    Bundles.get(NamedObject.class, NamedObject.META_TYPES_FOR_BUNDLE[type]), false));
+                                    Bundles.get(NamedObject.class, NamedObject.META_TYPES_FOR_BUNDLE[type]), ComparerTreeNode.TYPE_FOLDER));
 
                     loggingOutputPanel.append(MessageFormat.format("\n============= {0} to DROP  =============",
                             Bundles.getEn(NamedObject.class, NamedObject.META_TYPES_FOR_BUNDLE[type])));
@@ -838,7 +838,7 @@ public class ComparerDBPanel extends JPanel implements TabView {
 
             ComparerTreeNode typeNode = (ComparerTreeNode) actionNode.getChildByType(type);
             if (typeNode != null)
-                typeNode.add(new ComparerTreeNode(action, type, name, true));
+                typeNode.add(new ComparerTreeNode(action, type, name, ComparerTreeNode.COMPONENT));
         }
 
     }
@@ -901,29 +901,41 @@ public class ComparerDBPanel extends JPanel implements TabView {
 
     public static class ComparerTreeNode extends DefaultMutableTreeNode {
 
+        // --- Action constraints ---
+
         public static final int CREATE = 0;
         public static final int ALTER = CREATE + 1;
         public static final int DROP = ALTER + 1;
 
+        // --- Level constraints ---
+
+        public static final int ROOT = 0;
+        public static final int TYPE_FOLDER = ROOT + 1;
+        public static final int COMPONENT = TYPE_FOLDER + 1;
+
+        // ---
+
         private final boolean isComponent;
+        private final int level;
         private final int action;
         private final int type;
         private final String name;
 
         public ComparerTreeNode(String name) {
-            this(-1, -1, name, false);
+            this(-1, -1, name, ROOT);
         }
 
         public ComparerTreeNode(int action, String name) {
-            this(action, -1, name, false);
+            this(action, -1, name, ROOT);
         }
 
-        public ComparerTreeNode(int action, int type, String name, boolean isComponent) {
+        public ComparerTreeNode(int action, int type, String name, int level) {
             super();
             this.action = action;
             this.type = type;
             this.name = name;
-            this.isComponent = isComponent;
+            this.level = level;
+            this.isComponent = (level == COMPONENT);
         }
 
         public TreeNode getChildByAction(int type) {
@@ -1040,7 +1052,18 @@ public class ComparerDBPanel extends JPanel implements TabView {
                     break;
 
             }
-            setText(treeNode.name);
+
+            Font font = getFont();
+            if (treeNode.level == ComparerTreeNode.TYPE_FOLDER && treeNode.getChildCount() > 0) {
+                setText(treeNode.name + " (" + treeNode.getChildCount() + ")");
+                if (font != null)
+                    setFont(font.deriveFont(Font.BOLD));
+
+            } else {
+                setText(treeNode.name);
+                if (font != null)
+                    setFont(font.deriveFont(Font.PLAIN));
+            }
 
             return this;
         }

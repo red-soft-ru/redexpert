b07667f421bd137f3cb27199d626f72ee627ef82
aleksey.kozlov <aleksey.kozlov@red-soft.ru>
2023-10-19 12:15:17 +0300

    Fix UpdateLoader process will not end after updating (RS-151338)

--- src/org/executequery/UpdateLoader.java
+++ src/org/executequery/UpdateLoader.java
@@ -656,6 +656,8 @@ public class UpdateLoader extends JFrame {
         updateLoader.replaceFiles();
         if (launch)
             updateLoader.launch();
+        else
+            ExecuteQuery.stop();
     }
 
 }

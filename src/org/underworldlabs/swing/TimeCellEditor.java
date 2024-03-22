46708670bcc3186f0b20a58dcf6b23a41e981855
aleksey.kozlov <aleksey.kozlov@red-soft.ru>
2024-03-22 11:40:08 +0300

    Add TimeCellEditor width ajusting

--- src/org/underworldlabs/swing/TimeCellEditor.java
+++ src/org/underworldlabs/swing/TimeCellEditor.java
@@ -15,23 +15,32 @@ import java.util.EventObject;
 public class TimeCellEditor extends AbstractCellEditor implements TableCellEditor {
     private final EQTimePicker picker;
     private JTable table;
+    private int col;
 
     private final boolean autoAdjustMinimumTableRowHeight;
+    private final boolean autoAdjustMinimumTableColWidth;
     private final int minimumRowHeightInPixels;
+    private final int minimumColWidthInPixels;
     private int oldRowHeightInPixels;
+    private int oldColWidthInPixels;
 
     public TimeCellEditor() {
         picker = new EQTimePicker();
+
         this.autoAdjustMinimumTableRowHeight = true;
+        this.autoAdjustMinimumTableColWidth = true;
         this.minimumRowHeightInPixels = picker.getPreferredSize().height + 1;
+        this.minimumColWidthInPixels = picker.getPreferredSize().width + 1;
     }
 
     @Override
-    public Component getTableCellEditorComponent(JTable table, Object o, boolean b, int i, int i1) {
+    public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
         this.table = table;
+        this.col = column;
 
-        setCellEditorValue(((RecordDataItem) o).getDisplayValue());
+        setCellEditorValue(((RecordDataItem) value).getDisplayValue());
         zAdjustTableRowHeightIfNeeded();
+        zAdjustTableColWidthIfNeeded();
         picker.setMinimumSize(new Dimension(0, 0));
 
         return picker;
@@ -89,8 +98,24 @@ public class TimeCellEditor extends AbstractCellEditor implements TableCellEdito
         }
     }
 
+    private void zAdjustTableColWidthIfNeeded() {
+        this.oldColWidthInPixels = table.getColumnModel().getColumn(col).getWidth();
+
+        if (this.autoAdjustMinimumTableColWidth) {
+            if (table.getColumnModel().getColumn(col).getWidth() < this.minimumColWidthInPixels) {
+                table.getColumnModel().getColumn(col).setWidth(this.minimumColWidthInPixels);
+                table.getColumnModel().getColumn(col).setPreferredWidth(this.minimumColWidthInPixels);
+            }
+        }
+    }
+
     private void zRestoreTableRowHeigh() {
         table.setRowHeight(this.oldRowHeightInPixels);
+    }
+
+    private void zRestoreTableColWidth() {
+        table.getColumnModel().getColumn(col).setWidth(this.oldColWidthInPixels);
+        table.getColumnModel().getColumn(col).setPreferredWidth(this.oldColWidthInPixels);
     }
 
     public EQTimePicker getPicker() {
@@ -99,6 +124,7 @@ public class TimeCellEditor extends AbstractCellEditor implements TableCellEdito
 
     public void restoreCellSize() {
         zRestoreTableRowHeigh();
+        zRestoreTableColWidth();
     }
 
 }

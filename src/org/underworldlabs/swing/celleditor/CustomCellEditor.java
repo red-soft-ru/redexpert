18630b417a42667fc94fd63fe022f6406c521b2f
aleksey.kozlov <aleksey.kozlov@red-soft.ru>
2024-04-09 14:40:02 +0300

    Fix old value displaying when editing cell value (RS-172224)

--- src/org/underworldlabs/swing/celleditor/CustomCellEditor.java
+++ src/org/underworldlabs/swing/celleditor/CustomCellEditor.java
@@ -1,14 +1,18 @@
 package org.underworldlabs.swing.celleditor;
 
+import org.executequery.gui.resultset.RecordDataItem;
 import org.underworldlabs.swing.celleditor.picker.DefaultPicker;
 
 import javax.swing.*;
+import javax.swing.table.TableCellEditor;
+import java.awt.*;
 
-public class CustomCellEditor extends DefaultCellEditor {
+public class CustomCellEditor extends AbstractCellEditor
+        implements TableCellEditor {
+
     private final DefaultPicker picker;
 
     public CustomCellEditor(DefaultPicker picker) {
-        super(picker.getEditorComponent());
         this.picker = picker;
     }
 
@@ -16,4 +20,14 @@ public class CustomCellEditor extends DefaultCellEditor {
     public Object getCellEditorValue() {
         return picker.getValue();
     }
+
+    @Override
+    public Component getTableCellEditorComponent(JTable table, Object value, boolean isSelected, int row, int column) {
+
+        if (value instanceof RecordDataItem)
+            picker.setValue(((RecordDataItem) value).getNewValue());
+
+        return picker.getEditorComponent();
+    }
+
 }

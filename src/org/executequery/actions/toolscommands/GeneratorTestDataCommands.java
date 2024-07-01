c6edd3b88df4c63b94f3e9a2e562b8d813f01191
aleksey.kozlov <aleksey.kozlov@red-soft.ru>
2024-07-01 17:00:31 +0300

    Add icon for GeneratorTestDataPanel tab

--- src/org/executequery/actions/toolscommands/GeneratorTestDataCommands.java
+++ src/org/executequery/actions/toolscommands/GeneratorTestDataCommands.java
@@ -7,33 +7,43 @@ import org.executequery.gui.browser.GeneratorTestDataPanel;
 import org.executequery.gui.browser.GrantManagerPanel;
 import org.executequery.localization.Bundles;
 import org.executequery.repository.DatabaseConnectionRepository;
+import org.executequery.repository.Repository;
 import org.executequery.repository.RepositoryCache;
 import org.underworldlabs.swing.actions.BaseCommand;
 
-import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.util.List;
 
 public class GeneratorTestDataCommands extends OpenFrameCommand implements BaseCommand {
 
+    @Override
     public void execute(ActionEvent e) {
-        boolean execute_w = false;
-        List<DatabaseConnection> listConnections = ((DatabaseConnectionRepository) RepositoryCache.load(DatabaseConnectionRepository.REPOSITORY_ID)).findAll();
-        for (DatabaseConnection dc : listConnections) {
-            if (dc.isConnected()) {
-                execute_w = true;
-                break;
-            }
+        boolean hasActiveConnection = false;
+
+        Repository repo = RepositoryCache.load(DatabaseConnectionRepository.REPOSITORY_ID);
+        if (repo instanceof DatabaseConnectionRepository) {
+            List<DatabaseConnection> listConnections = ((DatabaseConnectionRepository) repo).findAll();
+            hasActiveConnection = listConnections.stream().anyMatch(DatabaseConnection::isConnected);
         }
-        if (execute_w) {
-            if (GUIUtilities.getCentralPane(GeneratorTestDataPanel.TITLE) == null)
-                GUIUtilities.addCentralPane(GeneratorTestDataPanel.TITLE,
-                        (Icon) null,
-                        new GeneratorTestDataPanel(),
-                        null,
-                        true);
-            else GUIUtilities.setSelectedCentralPane(GeneratorTestDataPanel.TITLE);
-        } else
+
+        if (!hasActiveConnection) {
             GUIUtilities.displayErrorMessage(Bundles.get(GrantManagerPanel.class, "message.notConnected"));
+            return;
+        }
+
+        String frameTitle = GeneratorTestDataPanel.TITLE;
+        if (GUIUtilities.getCentralPane(frameTitle) != null) {
+            GUIUtilities.setSelectedCentralPane(frameTitle);
+            return;
+        }
+
+        GUIUtilities.addCentralPane(
+                frameTitle,
+                GeneratorTestDataPanel.FRAME_ICON,
+                new GeneratorTestDataPanel(),
+                null,
+                true
+        );
     }
+
 }

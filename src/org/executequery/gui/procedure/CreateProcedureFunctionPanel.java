dc838e202c7b09017ddfb4427c4ca375dff2e8df
mikhail.kalyashin <mikhail.kalyashin@red-soft.ru>
2024-08-23 10:56:42 +0300

    Revert "Fix remove empty row when there are no any variables or cursors in the corresponding tables (RS-187856)"
    This reverts commit 66339f2ee11cb80f48f4360f08e385c1c03b4299.

--- src/org/executequery/gui/procedure/CreateProcedureFunctionPanel.java
+++ src/org/executequery/gui/procedure/CreateProcedureFunctionPanel.java
@@ -310,6 +310,10 @@ public abstract class CreateProcedureFunctionPanel extends AbstractCreateExterna
 
     private void loadVariables() {
 
+        // remove first empty row
+        variablesPanel.clearRows();
+        cursorsPanel.clearRows();
+
         String fullProcedureBody = getFullSourceBody();
         if (fullProcedureBody != null && !fullProcedureBody.isEmpty()) {
 

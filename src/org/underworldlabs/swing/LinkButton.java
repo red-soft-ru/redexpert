acf5626ecb44b200b3857b63b69dfc692b76c5bd
aleksey.kozlov <aleksey.kozlov@red-soft.ru>
2024-09-03 09:14:01 +0300

    Add class for painting JComponents on hover action

--- src/org/underworldlabs/swing/LinkButton.java
+++ src/org/underworldlabs/swing/LinkButton.java
@@ -20,13 +20,10 @@
 
 package org.underworldlabs.swing;
 
-import org.underworldlabs.swing.plaf.UIUtils;
+import org.underworldlabs.swing.listener.PaintMouseHandler;
 
 import javax.swing.*;
-import javax.swing.border.Border;
 import java.awt.*;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 
 /**
  * Simple button behaving/looking like a hyperlink item.
@@ -34,10 +31,6 @@ import java.awt.event.MouseEvent;
  * @author Takis Diakoumis
  */
 public class LinkButton extends JButton {
-
-    private static final Color LINK_COLOR = UIUtils.getColour("executequery.LinkButton.foreground", new Color(0x1155CC));
-    private static final Border LINK_BORDER = BorderFactory.createEmptyBorder(0, 0, 1, 0);
-    private static final Border HOVER_BORDER = BorderFactory.createMatteBorder(0, 0, 1, 0, LINK_COLOR);
 
     public LinkButton(String text) {
         super(text);
@@ -50,32 +43,11 @@ public class LinkButton extends JButton {
     }
 
     private void init() {
-        setBorder(null);
-        setBorder(LINK_BORDER);
-        setForeground(LINK_COLOR);
-        setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
         setFocusPainted(false);
-        setRequestFocusEnabled(false);
         setContentAreaFilled(false);
-        addMouseListener(new LinkMouseListener());
+        setRequestFocusEnabled(false);
+        setCursor(Cursor.getPredefinedCursor(Cursor.HAND_CURSOR));
+        addMouseListener(new PaintMouseHandler(this, PaintMouseHandler.Preset.COLORED_UNDERLINE));
     }
-
-    private class LinkMouseListener extends MouseAdapter {
-        public void mouseEntered(MouseEvent e) {
-            ((JComponent) e.getComponent()).setBorder(HOVER_BORDER);
-        }
-
-        public void mouseReleased(MouseEvent e) {
-            ((JComponent) e.getComponent()).setBorder(HOVER_BORDER);
-        }
-
-        public void mouseExited(MouseEvent e) {
-            ((JComponent) e.getComponent()).setBorder(LINK_BORDER);
-        }
-    }
-
-    ;
 
 }
-
-

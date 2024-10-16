188848c1c5663101b4a3744f62a89ded33fb3017
mikhail.kalyashin <mikhail.kalyashin@red-soft.ru>
2024-10-16 14:17:06 +0300

    See RS-198429 fix processing constraints for cell

--- src/org/underworldlabs/swing/celleditor/picker/NumberPicker.java
+++ src/org/underworldlabs/swing/celleditor/picker/NumberPicker.java
@@ -4,9 +4,11 @@ import org.executequery.databaseobjects.Types;
 import org.underworldlabs.Constants;
 
 import javax.swing.*;
+import javax.swing.text.AttributeSet;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DocumentFilter;
+import javax.swing.text.PlainDocument;
 import java.awt.*;
-import java.awt.event.KeyAdapter;
-import java.awt.event.KeyEvent;
 import java.math.BigInteger;
 
 /**
@@ -32,12 +34,14 @@ public class NumberPicker extends JTextField
         setHorizontalAlignment(JTextField.LEFT);
         setBorder(BorderFactory.createLineBorder(Color.DARK_GRAY, 1));
 
-        addKeyListener(new KeyAdapter() {
+        /*addKeyListener(new KeyAdapter() {
             @Override
             public void keyTyped(KeyEvent e) {
                 processKeyTyping(e);
             }
-        });
+        });*/
+        PlainDocument doc = (PlainDocument) getDocument();
+        doc.setDocumentFilter(new NumberFilter());
     }
 
     @Override
@@ -55,16 +59,10 @@ public class NumberPicker extends JTextField
         return this;
     }
 
-    private void processKeyTyping(KeyEvent evt) {
-
-        if (evt.getKeyChar() == KeyEvent.VK_BACK_SPACE || evt.getKeyChar() == KeyEvent.VK_DELETE)
-            return;
-        if (String.valueOf(evt.getKeyChar()).equals("-") && getCaretPosition() == 0)
-            return;
+    protected boolean checkValue(String value) {
 
         try {
-            String value = String.valueOf(new StringBuilder(getText().trim())
-                    .insert(getCaretPosition(), evt.getKeyChar()));
+
 
             switch (numberType) {
 
@@ -86,10 +84,38 @@ public class NumberPicker extends JTextField
                         throw new NumberFormatException();
                     break;
             }
+            return true;
 
         } catch (NumberFormatException ex) {
-            evt.consume();
+            return false;
         }
     }
+
+    class NumberFilter extends DocumentFilter {
+        public void insertString(FilterBypass fb, int offset, String string,
+                                 AttributeSet attr) throws BadLocationException {
+            StringBuilder sb = new StringBuilder(getText());
+            sb.insert(offset, string);
+            if (checkValue(sb.toString()))
+                super.insertString(fb, offset, string, attr);
+        }
+
+        public void replace(FilterBypass fb, int offset, int length, String string,
+                            AttributeSet attrs) throws BadLocationException {
+            StringBuilder sb = new StringBuilder(getText());
+            sb.replace(offset, offset + length, string);
+            if (checkValue(sb.toString()))
+                super.replace(fb, offset, length, string, attrs);
+        }
+
+        @Override
+        public void remove(FilterBypass fb, int offset, int length) throws BadLocationException {
+            StringBuilder sb = new StringBuilder(getText());
+            sb.delete(offset, offset + length);
+            if (checkValue(sb.toString()))
+                super.remove(fb, offset, length);
+        }
+    }
+
 
 }

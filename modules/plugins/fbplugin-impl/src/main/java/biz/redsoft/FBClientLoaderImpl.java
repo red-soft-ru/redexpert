27321fa0b16e9a5d8fffc658258c48e756d1d266
aleksey.kozlov <aleksey.kozlov@red-soft.ru>
2024-11-02 10:12:06 +0300

    Fix loading fbclient library for the Jaybird 3 (RS-199481)

--- modules/plugins/fbplugin-impl/src/main/java/biz/redsoft/FBClientLoaderImpl.java
+++ modules/plugins/fbplugin-impl/src/main/java/biz/redsoft/FBClientLoaderImpl.java
@@ -4,7 +4,6 @@ import com.sun.jna.Library;
 import com.sun.jna.Native;
 import com.sun.jna.NativeLibrary;
 import com.sun.jna.Platform;
-import org.firebirdsql.gds.ng.jna.NativeLibraryLoadException;
 import org.firebirdsql.gds.ng.jna.NativeResourceTracker;
 import org.firebirdsql.jna.fbclient.FbClientLibrary;
 import org.firebirdsql.jna.fbclient.WinFbClientLibrary;
@@ -13,7 +12,6 @@ import org.firebirdsql.logging.LoggerFactory;
 
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Proxy;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -46,35 +44,21 @@ public class FBClientLoaderImpl implements IFBClientLoader {
     }
 
     private FbClientLibrary createClientLibrary() {
-
-        final List<Throwable> throwablesList = new ArrayList<>();
         final List<String> librariesToTry = Arrays.asList("fbembed", "fbclient");
 
         for (String libraryName : librariesToTry) {
             try {
-                if (Platform.isWindows()) {
+                if (Platform.isWindows())
                     return Native.load(libraryName, WinFbClientLibrary.class);
-                } else {
+                else
                     return Native.load(libraryName, FbClientLibrary.class);
-                }
 
-            } catch (RuntimeException | UnsatisfiedLinkError e) {
-                throwablesList.add(e);
+            } catch (UnsatisfiedLinkError e) {
                 log.debug("Attempt to load " + libraryName + " failed", e);
-                // continue with next
             }
         }
 
-        assert throwablesList.size() == librariesToTry.size();
-        log.error("Could not load any of the libraries in " + librariesToTry + ":");
-
-        for (int idx = 0; idx < librariesToTry.size(); idx++)
-            log.error("Loading " + librariesToTry.get(idx) + " failed", throwablesList.get(idx));
-
-        throw new NativeLibraryLoadException(
-                "Could not load any of " + librariesToTry + "; linking first exception",
-                throwablesList.get(0)
-        );
+        return null;
     }
 
 }

8e20b6086a50510bae512b976b0a4667e9baf415
aleksey.kozlov <aleksey.kozlov@red-soft.ru>
2024-11-15 09:25:28 +0300

    Refactor LoginPasswordDialog class

--- src/org/executequery/gui/LoginPasswordDialog.java
+++ src/org/executequery/gui/LoginPasswordDialog.java
@@ -1,219 +1,159 @@
 package org.executequery.gui;
 
-import org.executequery.gui.browser.ConnectionPanel;
 import org.executequery.localization.Bundles;
-import org.underworldlabs.swing.LinkButton;
+import org.underworldlabs.swing.LinkLabel;
+import org.underworldlabs.swing.ViewablePasswordField;
 import org.underworldlabs.swing.layouts.GridBagHelper;
 
 import javax.swing.*;
-import javax.swing.event.AncestorEvent;
-import javax.swing.event.AncestorListener;
-import java.awt.*;
 import java.awt.event.*;
-import java.net.URL;
 
 public class LoginPasswordDialog extends BaseDialog {
-    private JTextField username;
-    private JPasswordField password;
-    private JCheckBox storePassword;
-    private JCheckBox showPassword;
+
     private final String message;
-    private final String user;
+    private final String username;
+    private final String registrationUrl;
+
     private boolean closedDialog = false;
-    private final String urlOfRegistration;
 
-    public LoginPasswordDialog(String name, String message, String urlOfRegistration) {
-        this(name, message, urlOfRegistration, null);
+    // --- GUI components ---
+
+    private JButton submitButton;
+    private JButton cancelButton;
+    private LinkLabel linkButton;
+
+    private JPanel mainPanel;
+    private JTextField usernameField;
+    private JCheckBox storePasswordCheck;
+    private ViewablePasswordField passwordField;
+
+    // ---
+
+    public LoginPasswordDialog(String name, String message, String registrationUrl) {
+        this(name, message, registrationUrl, null);
     }
 
-    public LoginPasswordDialog(String name, String message, String urlOfRegistration, String username) {
+    public LoginPasswordDialog(String name, String message, String registrationUrl, String username) {
         super(name, true, true);
+        this.registrationUrl = registrationUrl;
+        this.username = username;
         this.message = message;
-        user = username;
-        this.urlOfRegistration = urlOfRegistration;
+
         init();
+        arrange();
+        initListeners();
     }
 
-    void init() {
-        JPanel mainPanel = new JPanel(new GridBagLayout());
-        JTextPane pane = new JTextPane();
-        pane.setEditable(false);
-        pane.setText(message);
-        username = new JTextField();
-        if (user != null) {
-            username.setText(user);
-        }
-        username.addKeyListener(new KeyAdapter() {
-            @Override
-            public void keyReleased(KeyEvent e) {
-                if (e.getKeyCode() == KeyEvent.VK_ENTER)
-                    password.requestFocusInWindow();
-            }
-        });
-        password = new JPasswordField();
-        password.addKeyListener(new KeyAdapter() {
-            @Override
-            public void keyReleased(KeyEvent e) {
-                if (e.getKeyCode() == KeyEvent.VK_ENTER)
-                    finished();
-            }
-        });
-        storePassword = new JCheckBox(Bundles.get(ConnectionPanel.class, "StorePassword"));
-        storePassword.addKeyListener(new KeyAdapter() {
-            @Override
-            public void keyReleased(KeyEvent e) {
-                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
-                    storePassword.setSelected(!storePassword.isSelected());
-                }
-            }
-        });
-        showPassword = new JCheckBox(Bundles.get(ConnectionPanel.class, "ShowPassword"));
+    private void init() {
 
-        showPassword.addItemListener(new ItemListener() {
-            public void itemStateChanged(ItemEvent e) {
-                if (e.getStateChange() == ItemEvent.SELECTED) {
-                    password.setEchoChar((char) 0);
-                } else {
-                    password.setEchoChar('•');
-                }
-            }
-        });
-        showPassword.addKeyListener(new KeyAdapter() {
-            @Override
-            public void keyReleased(KeyEvent e) {
-                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
-                    showPassword.setSelected(!showPassword.isSelected());
-                }
-            }
-        });
-        LinkButton linkButton = new LinkButton(bundledString("register"));
-        linkButton.addKeyListener(new KeyAdapter() {
-            @Override
-            public void keyReleased(KeyEvent e) {
-                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
-                    try {
-                        Desktop.getDesktop().browse(new URL(urlOfRegistration).toURI());
-                    } catch (Exception ex) {
-                        ex.printStackTrace();
-                    }
-                }
-            }
-        });
-        linkButton.addMouseListener(new MouseAdapter() {
-            @Override
-            public void mouseClicked(MouseEvent e) {
-                try {
-                    Desktop.getDesktop().browse(new URL(urlOfRegistration).toURI());
-                } catch (Exception ex) {
-                    ex.printStackTrace();
-                }
-            }
-        });
-        linkButton.setVisible(urlOfRegistration != null);
-        storePassword.setVisible(urlOfRegistration == null);
-        JButton okButton = new JButton(bundledString("login"));
-        okButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                finished();
-            }
-        });
-        okButton.addKeyListener(new KeyAdapter() {
-            @Override
-            public void keyReleased(KeyEvent e) {
-                if (e.getKeyCode() == KeyEvent.VK_ENTER)
-                    finished();
-            }
-        });
-        JButton cancelButton = new JButton(Bundles.getCommon("cancel.button"));
-        cancelButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent actionEvent) {
-                setClosedDialog(true);
-                finished();
-            }
-        });
-        cancelButton.addKeyListener(new KeyAdapter() {
-            @Override
-            public void keyReleased(KeyEvent e) {
-                if (e.getKeyCode() == KeyEvent.VK_ENTER) {
-                    setClosedDialog(true);
-                    finished();
-                }
-            }
-        });
-        ActionListener escListener = new ActionListener() {
+        mainPanel = WidgetFactory.createPanel("mainPanel");
+        usernameField = WidgetFactory.createTextField("usernameField", username);
+        passwordField = WidgetFactory.createViewablePasswordField("passwordField");
 
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                setClosedDialog(true);
-                finished();
-            }
-        };
-        mainPanel.registerKeyboardAction(escListener,
-                KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0),
-                JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
+        submitButton = WidgetFactory.createButton("submitButton", bundledString("login"), e -> finished());
+        linkButton = WidgetFactory.createLinkLabel("linkButton", bundledString("register"), registrationUrl);
+        cancelButton = WidgetFactory.createButton("submitButton", Bundles.getCommon("cancel.button"), e -> cancel());
+        storePasswordCheck = WidgetFactory.createCheckBox("storePasswordCheck", Bundles.get("AbstractConnectionPanel.StorePassword"));
 
-        GridBagHelper gbh = new GridBagHelper();
-        gbh.setDefaultsStatic();
-        gbh.defaults();
-        mainPanel.add(pane, gbh.spanX().get());
-        gbh.addLabelFieldPair(mainPanel, bundledString("username"), username, null);
+        linkButton.setVisible(registrationUrl != null);
+        storePasswordCheck.setVisible(registrationUrl == null);
+    }
 
-        gbh.addLabelFieldPair(mainPanel, bundledString("password"), password, null);
+    private void cancel() {
+        setClosedDialog(true);
+        finished();
+    }
 
-        mainPanel.add(password, gbh.nextCol().fillHorizontally().spanX().get());
+    private void arrange() {
+        GridBagHelper gbh;
 
-        mainPanel.add(showPassword, gbh.nextRow().setLabelDefault().get());
-        mainPanel.add(storePassword, gbh.nextCol().get());
+        // --- button panel ---
 
+        JPanel buttonPanel = WidgetFactory.createPanel("buttonPanel");
 
-        mainPanel.add(linkButton, gbh.nextRowFirstCol().get());
+        gbh = new GridBagHelper().anchorNorthWest().fillHorizontally();
+        buttonPanel.add(submitButton, gbh.get());
+        buttonPanel.add(cancelButton, gbh.nextCol().leftGap(5).get());
 
-        mainPanel.add(okButton, gbh.nextCol().anchorEast().get());
-        mainPanel.add(cancelButton, gbh.nextCol().anchorNorthWest().fillHorizontally().setMaxWeightX().get());
+        // --- main panel ---
+
+        gbh = new GridBagHelper().anchorNorthWest().setInsets(5, 5, 5, 0).fillHorizontally();
+        mainPanel.add(WidgetFactory.createLabel(format(message)), gbh.setMaxWeightY().spanX().get());
+        mainPanel.add(WidgetFactory.createLabel(bundledString("username")), gbh.nextRowFirstCol().setWidth(1).setMinWeightY().setMinWeightX().get());
+        mainPanel.add(usernameField, gbh.nextCol().leftGap(0).setMaxWeightX().get());
+        mainPanel.add(WidgetFactory.createLabel(bundledString("password")), gbh.nextRowFirstCol().leftGap(5).setMinWeightX().get());
+        mainPanel.add(passwordField, gbh.nextCol().leftGap(0).setMaxWeightX().get());
+        mainPanel.add(storePasswordCheck, gbh.nextRow().get());
+        mainPanel.add(linkButton, gbh.get());
+        mainPanel.add(buttonPanel, gbh.nextRowFirstCol().leftGap(5).bottomGap(5).spanX().get());
 
         setResizable(false);
         addDisplayComponent(mainPanel);
-        mainPanel.addAncestorListener(new AncestorListener() {
+    }
+
+    private void initListeners() {
+
+        usernameField.addKeyListener(new KeyAdapter() {
             @Override
-            public void ancestorAdded(AncestorEvent event) {
-                if (user == null) {
-                    username.requestFocusInWindow();
-                } else {
-                    password.requestFocusInWindow();
-                }
-            }
-
-            @Override
-            public void ancestorRemoved(AncestorEvent event) {
-
-            }
-
-            @Override
-            public void ancestorMoved(AncestorEvent event) {
-
+            public void keyReleased(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER)
+                    passwordField.getField().requestFocusInWindow();
             }
         });
+
+        passwordField.getField().addKeyListener(new KeyAdapter() {
+            @Override
+            public void keyReleased(KeyEvent e) {
+                if (e.getKeyCode() == KeyEvent.VK_ENTER)
+                    finished();
+            }
+        });
+
+        mainPanel.registerKeyboardAction(
+                e -> cancel(),
+                KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0),
+                JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT
+        );
+
         addWindowListener(new WindowAdapter() {
             @Override
             public void windowClosing(WindowEvent e) {
-                setClosedDialog(true);
-                finished();
+                cancel();
             }
         });
 
     }
 
+    // --- helper methods ---
+
+    private String format(String message) {
+
+        if (message == null)
+            return "";
+
+        String formatted = message.replace("\n", "<br>");
+        formatted = String.format("<html>%s</html>", formatted);
+
+        return formatted;
+    }
+
+    private static String bundledString(String key) {
+        return Bundles.get(LoginPasswordDialog.class, key);
+    }
+
+    // --- getters/setters ---
+
     public String getUsername() {
-        return username.getText();
+        return usernameField.getText();
     }
 
     public String getPassword() {
-        return new String(password.getPassword());
+        return passwordField.getPassword();
     }
 
-    public boolean isStorePassword(){return storePassword.isSelected();}
+    public boolean isStorePassword() {
+        return storePasswordCheck.isSelected();
+    }
 
     public boolean isClosedDialog() {
         return closedDialog;
@@ -223,7 +163,4 @@ public class LoginPasswordDialog extends BaseDialog {
         this.closedDialog = closedDialog;
     }
 
-    private String bundledString(String key) {
-        return Bundles.get(this.getClass(), key);
-    }
 }
